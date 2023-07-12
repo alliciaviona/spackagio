2120d0458713b972c77cc406bebc071e9e542405
Allicia Viona <alliciawn@gmail.com>
2023-07-12 15:21:35 +0700

    Initial Commit

---
+++ Usualv2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Usualv2
+//
+//  Created by Allicia Viona Sagi on 12/07/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Usualv2/Usualv2App.swift
@@ -0,0 +1,17 @@
+//
+//  Usualv2App.swift
+//  Usualv2
+//
+//  Created by Allicia Viona Sagi on 12/07/23.
+//
+
+import SwiftUI
+
+@main
+struct Usualv2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
